3439694f3f60863a85c95ceb2cff81895601ccb1
Rufino John <aguilarufino@gmail.com>
2023-11-08 09:48:30 +0800

    added view for authors, post, dashboard

---
+++ app/Views/authors/index.php
@@ -0,0 +1,5 @@
+<?= $this->extend('template/admin_template'); ?>
+
+<?= $this->section('content'); ?>
+<h1>AUTHORS PAGE</h1>
+<?= $this->endSection(); ?>
---
+++ app/Views/posts/index.php
@@ -0,0 +1,5 @@
+<?= $this->extend('template/admin_template'); ?>
+
+<?= $this->section('content'); ?>
+<h1>POSTS PAGE</h1>
+<?= $this->endSection(); ?>
